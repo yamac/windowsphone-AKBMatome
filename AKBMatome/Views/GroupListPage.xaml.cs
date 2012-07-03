a3c163b50ac435ed6c1cfc6da8ec3086cd0908a7
yamac <dev@yamac.net>
2012-07-04 03:24:19 +0900

    Add group

---
+++ AKBMatome/Views/GroupListPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Phone.Controls;
+
+namespace AKBMatome.Views
+{
+    public partial class GroupListPage : PhoneApplicationPage
+    {
+        public GroupListPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
